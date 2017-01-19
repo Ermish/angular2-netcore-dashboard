f9403ae705ee0894a418e9949d58e3491438617a
Philip Ermish <philip.ermish@amwins.com>
2017-01-19 14:50:24 -0500

    building out configs and project layout

--- src/Dashboard/gulpfile.js
+++ src/Dashboard/gulpfile.js
@@ -4,60 +4,71 @@
 'use strict';
 
 var chalk = require('chalk'),
+    concat = require('gulp-concat'),
     del = require('del'),
     es = require('event-stream'),
     filter = require('gulp-filter'),
     gulp = require('gulp'),
     gulpif = require('gulp-if'),
-    path = require('path'),
-    tap = require('gulp-tap'),
-
-    autoprefixer = require('autoprefixer'),
-    concat = require('gulp-concat'),
-    cssmin = require('gulp-cssmin'),
-    htmlmin = require('gulp-htmlmin'),
     logger = require('gulp-logger'),
-    postcss = require('gulp-postcss'),
-    htmlpostcss = require('gulp-html-postcss'),
-    rename = require('gulp-rename'),
+    path = require('path'),
     rev = require('gulp-rev'),
     revReplace = require('gulp-rev-replace'),
     RevAll = require('gulp-rev-all'),
-    sass = require('gulp-sass'),
+    rename = require('gulp-rename'),
+    tap = require('gulp-tap'),
+
+    //angular 2
+
+    //js
+    typescript = require('gulp-typescript'),
+    sourcemaps = require('gulp-sourcemaps'),
+    typescriptProj = typescript.createProject('./tsconfig.json'),
+    tslint = require('gulp-tslint'),
     uglify = require('gulp-uglify'),
-    webpack = require('gulp-webpack');
+    webpack = require('gulp-webpack'),
+
+    //css
+    autoprefixer = require('autoprefixer'),
+    cssmin = require('gulp-cssmin'),
+    postcss = require('gulp-postcss'),
+    sass = require('gulp-sass'),
+
+    //html
+    htmlmin = require('gulp-htmlmin'),
+    htmlpostcss = require('gulp-html-postcss');
 
 
 var cleanAll = function (cb) {
-    del.sync(['./wwwroot/dist/**/*']);
-    console.log(chalk.cyan('Cleaned up wwwroot/dist folder'));
+    del.sync(['./wwwroot/**/*']);
+    console.log(chalk.cyan('Cleaned up wwwroot folder'));
     if (typeof cb === 'function')
         cb();
 }
 
 var cleanCss = function (cb) {
-    del.sync(['./wwwroot/dist/css/**/*']);
+    del.sync(['./wwwroot/css/**/*']);
     console.log(chalk.cyan('Cleaned up css folder'));
     if (typeof cb === 'function')
         cb();
 }
 
 var cleanJs = function (cb) {
-    del.sync(['./wwwroot/dist/js/**/*']);
+    del.sync(['./wwwroot/js/**/*']);
     console.log(chalk.cyan('Cleaned up js folder'));
     if (typeof cb === 'function')
         cb();
 }
 
 var cleanHtml = function (cb) {
-    del.sync(['./wwwroot/dist/html/**/*']);
+    del.sync(['./wwwroot/html/**/*']);
     console.log(chalk.cyan('Cleaned up views folder'));
     if (typeof cb === 'function')
         cb();
 }
 
 var processComponentsCss = function () {
-    return gulp.src('wwwroot/src/components/**/*.scss', { base: '.' })
+    return gulp.src('app/components/**/*.scss', { base: '.' })
         .pipe(logger({
             before: 'Starting to process scss files in components...',
             after: 'Done processing scss files!',
@@ -74,7 +85,7 @@ var processComponentsCss = function () {
 
 
 var processCss = function(isMinimal) {
-    return gulp.src('wwwroot/src/css/**/*.scss')
+    return gulp.src('app/css/**/*.scss')
         .pipe(logger({
             before: 'Starting to process scss files...',
             after: 'Done processing scss files!',
@@ -85,64 +96,69 @@ var processCss = function(isMinimal) {
             autoprefixer({ browsers: ['last 20 versions'] }), require('postcss-flexbugs-fixes')
         ]))
         .pipe(concat('app.css'))
-        .pipe(gulp.dest('wwwroot/dist/css')) //output app.css
+        .pipe(gulp.dest('wwwroot/css')) //output app.css
         .pipe(cssmin())
         .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest('wwwroot/dist/css')) //output app.min.css
-        .pipe(gulp.dest('wwwroot/dist/css')); //output app-123.min.css
+        .pipe(gulp.dest('wwwroot/css')) //output app.min.css
+        .pipe(gulp.dest('wwwroot/css')); //output app-123.min.css
 };
 
 var processJs = function(isMinimal) {
-    return gulp.src('wwwroot/src/js/app.js')
+    return gulp.src('app/js/app.js')
         .pipe(logger({
             before: 'Starting to process js files...',
             after: 'Done processing js files!',
             showChange: true
         }))
-        .pipe(webpack({
-            entry: {
-                app: './wwwroot/src/js/app.js'
-            },
-            output: {
-                library: 'foo',
-                libraryTarget: 'window',
-                filename: '[name].js'
-            },
-            module: {
-                loaders: [
-                    //{
-                    //    //exclude: /(node_modules|bower_components)/,
-                    //    loader: 'babel', // 'babel-loader' is also a valid name to reference
-                    //    query: {
-                    //        presets: ['es2015']
-                    //    }
-                    //}
-                ]
-            }
-        }))
-        .pipe(gulp.dest('wwwroot/dist/js')) //output app.js
-        .pipe(uglify())
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest('wwwroot/dist/js')) //output app.min.js
-        .pipe(gulp.dest('wwwroot/dist/js')); //output app-123.min.js
+        .pipe(sourcemaps.init())
+        .pipe(typescript(typescriptProj))
+        .pipe(sourcemaps.write('.'))      // <--- sourcemaps
+        .pipe(tslint())
+        .pipe(tslint.report('verbose'))
+        //.pipe(webpack({
+        //    entry: {
+        //        app: './app/js/app.js'
+        //    },
+        //    output: {
+        //        library: 'foo',
+        //        libraryTarget: 'window',
+        //        filename: '[name].js'
+        //    },
+        //    module: {
+        //        loaders: [
+        //            //{
+        //            //    //exclude: /(node_modules|bower_components)/,
+        //            //    loader: 'babel', // 'babel-loader' is also a valid name to reference
+        //            //    query: {
+        //            //        presets: ['es2015']
+        //            //    }
+        //            //}
+        //        ]
+        //    }
+        //}))
+        //.pipe(gulp.dest('wwwroot/js')) //output app.js
+        //.pipe(uglify())
+        //.pipe(rename({ suffix: '.min' }))
+        //.pipe(gulp.dest('wwwroot/js')) //output app.min.js
+        .pipe(gulp.dest('wwwroot/js')); //output app-123.min.js
 };
 
 var processHtml = function (isMinimal) {
-    var cacheBustingRevisionsManifest = "wwwroot/dist/rev-manifest.json";
+    var cacheBustingRevisionsManifest = "wwwroot/rev-manifest.json";
     var htmlFilter = filter('**/*.html', { restore: true });
     var jsFilter = filter('**/*.js', { restore: true });
 
     if (isMinimal) {
-        return gulp.src('wwwroot/src/html/**/*.html')
+        return gulp.src('app/html/**/*.html')
             .pipe(logger({
                 before: 'Starting to process html files...',
                 after: 'Done processing html files!',
                 showChange: true
             }))
-            .pipe(gulp.dest('wwwroot/dist/html'));
+            .pipe(gulp.dest('wwwroot/html'));
     }
     
-    return gulp.src('wwwroot/src/html/**/*.html')
+    return gulp.src('app/html/**/*.html')
         .pipe(logger({
             before: 'Starting to process html files...',
             after: 'Done processing hmtl files!',
@@ -168,9 +184,9 @@ var processHtml = function (isMinimal) {
         //.pipe(htmlFilter.restore)
         //.pipe(RevAll.revision({
         //    dontRenameFile: ['.html'],
-        //    //prefix: 'wwwroot/dist/html/',
+        //    //prefix: 'wwwroot/html/',
         //    replacer: function(fragment, replaceRegExp, newReference, referencedFile) {
-        //        var prefix = 'wwwroot/dist/html/';
+        //        var prefix = 'wwwroot/html/';
         //        var regExp = replaceRegExp;
                 
         //        if (referencedFile.path.match(/.js/) && replaceRegExp.toString().includes('.js')) {
@@ -179,17 +195,17 @@ var processHtml = function (isMinimal) {
         //        }
         //    }
         //}))
-        .pipe(gulp.dest('wwwroot/dist/html'));
+        .pipe(gulp.dest('wwwroot/html'));
 };
 
 var processImages = function () {
-    return gulp.src('wwwroot/src/images/**/*')//.src('wwwroot/src/html/**/*.html')
+    return gulp.src('app/images/**/*')//.src('app/html/**/*.html')
         .pipe(logger({
             before: 'Starting to process image files...',
             after: 'Processing image files complete!',
             showChange: true
         }))
-        .pipe(gulp.dest('wwwroot/dist/images'));
+        .pipe(gulp.dest('wwwroot/images'));
 };
 
 
@@ -197,36 +213,36 @@ var processImages = function () {
 
 
 /**
- * Watches for any changes, processes, and automatically updates the wwwroot/dist folder.
+ * Watches for any changes, processes, and automatically updates the wwwroot folder.
  */
 gulp.task('watch', function () {
-    gulp.watch('wwwroot/src/components/**/*.scss', function(cb) {
+    gulp.watch('app/components/**/*.scss', function(cb) {
         return processComponentsCss();
     });
 
-    gulp.watch('wwwroot/src/css/**/*.scss', function (cb) {
+    gulp.watch('app/css/**/*.scss', function (cb) {
         cleanCss(cb);
         return processCss(true);
     });
 
-    gulp.watch('wwwroot/src/**/*.js', function (cb) {
+    gulp.watch('app/**/*.js', function (cb) {
         cleanJs(cb);
         return processJs(true);
     });
 
-    gulp.watch('wwwroot/src/components/**/*.html', function (cb) {
+    gulp.watch('app/components/**/*.html', function (cb) {
         cleanHtml(cb);
         return processHtml(true);
     });
 
-    gulp.watch('wwwroot/src/html/**/*.html', function (cb) {
+    gulp.watch('app/html/**/*.html', function (cb) {
         cleanHtml(cb);
         return processHtml(true);
     });
 });
 
 /**
- * /This performs the same functionality as the watch tasks, but does a quick complete build that loads the 'wwwroot/dist' folder. 
+ * /This performs the same functionality as the watch tasks, but does a quick complete build that loads the 'wwwroot' folder. 
  * It does not vulcanize and does minimal optimization.
  */
 gulp.task('build-dev-lite', function(cb) {
@@ -244,7 +260,7 @@ gulp.task('build-dev-lite', function(cb) {
 });
 
 /**
- * /This performs complete build that loads the 'wwwroot/dist' folder. 
+ * /This performs complete build that loads the 'wwwroot' folder. 
  * This includes full optimization, cache-busting, vulcanization.
  */
 gulp.task('build-dev', function (cb) {
--- src/Dashboard/wwwroot/src/_references.js
+++
@@ -1,4 +0,0 @@
-﻿/// <autosync enabled="true" />
-/// <reference path="../../bower_components/jquery/dist/jquery.js" />
-/// <reference path="../../gulpfile.js" />
-/// <reference path="js/site.js" />
